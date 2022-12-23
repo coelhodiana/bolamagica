24183601ebbc3ce6c789abb040d74d696951468b
coelhodiana <coelhodiana04@gmail.com>
2022-12-23 10:29:58 -0400

    test: ajeitei o 'teste'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
-import {render, screen} from '@testing-library/angular'
-import { AppComponent } from './app.component'
+import { render, screen } from '@testing-library/angular';
+
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  test('should render title', async () => {
+  test('should render shake', async () => {
     await render(AppComponent, {})
 
-    expect(screen.getByText('Boilerplate Angular')).toBeInTheDocument()
+    expect(screen.getByText('shake')).toBeInTheDocument()
   })
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'bola mágica';
-
   answers = [
     'It is certain.',
     'It is decidedly so.',
