a496285b8ccbde0265ba9f8e005e195a9577f1a1
coelhodiana <coelhodiana04@gmail.com>
2022-12-23 10:29:22 -0400

    feat: esqueleto maluco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'boilerplate-angular';
+  title = 'bola mágica';
+
+  answers = [
+    'It is certain.',
+    'It is decidedly so.',
+    'Without a doubt.',
+    'Yes definitely.',
+    'You may rely on it.',
+    'As I see it, yes.',
+    'Most likely.',
+    'Outlook good.',
+    'Yes.',
+    'Signs point to yes.',
+    'Reply hazy, try again.',
+    'Ask again later.',
+    'Better not tell you now.',
+    'Cannot predict now.',
+    'Concentrate and ask again.',
+    "Don't count on it.",
+    'My reply is no.',
+    'My sources say no.',
+    'Outlook not so good.',
+    'Very doubtful.',
+  ];
+
+  randomNumber: number | null = null;
+
+  shake() {
+    this.randomNumber = Math.floor(Math.random() * (19 - 0 + 1) + 0);
+  }
 }
