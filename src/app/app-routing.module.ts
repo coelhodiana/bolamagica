dbf21e21b58f5c394539c301663df3a6e7b72b75
coelhodiana <coelhodiana04@gmail.com>
2023-02-09 19:14:49 -0400

    fix: route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { BallComponent } from './pages/ball/ball.component';
+import { GymComponent } from './pages/gym/gym.component';
 
 const routes: Routes = [
   {
     path: 'gym',
-    loadChildren: () => import('./pages/gym/gym.module').then(m => m.GymModule)
+    component: GymComponent
   },
   {
     path: '',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ShakeDirective } from './shared/directives/shake.directive';
 import { BallComponent } from './pages/ball/ball.component';
+import { GymComponent } from './pages/gym/gym.component';
+import { ShakeDirective } from './shared/directives/shake.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShakeDirective,
     BallComponent,
+    GymComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
